db3551ef0cbf9747a66a1cf0fa43df744e248e23
Anthony <106651735+Watai210@users.noreply.github.com>
2022-07-22 12:01:25 +0200

    .

--- CV/js/js.js
+++ CV/js/js.js
@@ -1,4 +1,16 @@
-function exp(exp){
+function exp(btnexp){
     const para = document.getElementById('paragraphe');
     para.setAttribute('class', 'experience');
+}
+function comp(btncomp){
+    const para = document.getElementById('paragraphe');
+    para.setAttribute('class', 'competences');
+}
+function forma(btnforma){
+    const para = document.getElementById('paragraphe');
+    para.setAttribute('class', 'formations');
+}
+function infos(btninfos){
+    const para = document.getElementById('paragraphe');
+    para.setAttribute('class', 'informations');
 }
