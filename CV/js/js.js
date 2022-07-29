cacd53d8441509a501126ba7e9ae4c882602f9a5
Watai210 <106651735+Watai210@users.noreply.github.com>
2022-07-30 01:19:07 +0200

    .

--- CV/js/js.js
+++ CV/js/js.js
@@ -1,18 +1,12 @@
 /*Fonctions boutons*/
 
 function exp(btnexp){
-    const para = document.getElementById('paragraphe');
-    para.setAttribute('class', 'experience');
-    const paraexp = document.getElementsByClassName('experience');
-    paraexp.innerHTML = "<p>Transport Amour<strong>| Stagiaire Assistant Logistique |</strong> Avril 2022<br><span class='taches'><u>Tâches réalisées :</u></span><br>Gestion des stocks, contacter les cleints, établir un ittinéraire, vérification de la conformité des livraisons, chargement des commandes dans les véhicules</p><br><p>A.I.P.S.Q <strong>| Ouvrier Polyvalent |</strong> Septembre 2021 –  Mai 2022<br><span class='taches'>Tâches réalisées :</span><br>Prises de mesures, évaluer les réparations, décaper, racler, réalisation d'assemblages simples, réfection d'éléments en bois et métal, poncer, teinter, peindre , vernir, entretien et nettoyage des outils mis a disposition ainsi que des locaux</p><br><p>SOPROCOS <strong>| Assistant Logistique |</strong> 2013<br><span class='taches'><u>Tâches réalisées :</u></span><br>Gestion des stocks et des approvisionnements de matières premières, contacter les fournisseurs, communiquer avec les transporteurs, assurer le suivi des livraisons et des commandes. utilisation régulière du logiciel SAP</p>";
+    document.getElementById('paragraphe').innerHTML = "<p>Transport Amour<strong>| Stagiaire Assistant Logistique |</strong> Avril 2022<br><span class='taches'><u>Tâches réalisées :</u></span><br>Gestion des stocks, contacter les cleints, établir un ittinéraire, vérification de la conformité des livraisons, chargement des commandes dans les véhicules</p><br><p>A.I.P.S.Q <strong>| Ouvrier Polyvalent |</strong> Septembre 2021 –  Mai 2022<br><span class='taches'>Tâches réalisées :</span><br>Prises de mesures, évaluer les réparations, décaper, racler, réalisation d'assemblages simples, réfection d'éléments en bois et métal, poncer, teinter, peindre , vernir, entretien et nettoyage des outils mis a disposition ainsi que des locaux</p><br><p>SOPROCOS <strong>| Assistant Logistique |</strong> 2013<br><span class='taches'><u>Tâches réalisées :</u></span><br>Gestion des stocks et des approvisionnements de matières premières, contacter les fournisseurs, communiquer avec les transporteurs, assurer le suivi des livraisons et des commandes. utilisation régulière du logiciel SAP</p>";
     const parabg = document.getElementById('fondpara');
     parabg.setAttribute('class', 'expbg');
 }
 function comp(btncomp){
-    const para = document.getElementById('paragraphe');
-    para.setAttribute('class', 'competences');
-    const paracomp = document.getElementsByClassName('competences');
-    paracomp.innerHTML = ""
+    document.getElementById('paragraphe').innerHTML = "<p class='compleft'>Maintenance de matériel informatique<br>Assistance aux utilisateurs<br>Support par prise en main a distance<br>Diagnostic et réparation de pannes<br>Recommandations liées au besoin du client</p><p class='compright'><br>Mise à jour de logiciels<br>Nettoyage physique d'ordinateurs<br>Suppression de virus<br>Installation et configuration d'équipements réseau (routeur, modem, répéteur, serveur NAS)<br>Installation et configuration d'ordinateurs (OS, drivers, antivirus, logiciels de bureautique...)<br>Maintenance et réparations sur appareils mobiles et tablettes</p>"
     const parabg = document.getElementById('fondpara');
     parabg.setAttribute('class', 'compbg');
 }
